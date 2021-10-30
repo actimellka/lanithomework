47dc9367248c6924878b752713e080b8c8699722
Ekaterina Ryzhenkova <vadivazikryzh@gmail.com>
2021-10-31 01:17:30 +0300

    homework1.1

---
+++ HomeWork1Library/HomeWork1Library/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+
+namespace HomeWork1Library
+{ 
+    public  class ReadFile
+    {
+        
+        public static void ReadFile1()
+        {
+           
+            Console.WriteLine("Enter the path to the file:");
+            string namefile = Console.ReadLine();
+
+
+
+            try
+            {
+                using (StreamReader file = new StreamReader(namefile))
+                {
+                    string data = file.ReadToEnd();
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(data);
+                }
+            }
+            catch
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You entered the wrong path.\n");
+            }
+        }
+    }  
+}
